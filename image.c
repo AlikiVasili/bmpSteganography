c49945497f8be6e6770e30bd2c5fc8cece31ded4
Panagiotis-Papadopoulos <ppapad03@ucy.ac.cy>
2020-11-18 14:11:29 +0200

    Minro bug fixes in image.c

--- image.c
+++ image.c
@@ -16,9 +16,9 @@ IMAGE * initImage(FILE *file,char *fileName){
 		return NULL;
 	}
 	image->header->bitmapfileheader = malloc(sizeof(byte)*14);	//Need to allocate space for bitmapfileheader
-	fread(image -> header -> bitmapfileheader,sizeof( image -> header -> bitmapfileheader), 14, file);
+	fread(image -> header -> bitmapfileheader,sizeof(byte), 14, file);
 	image->header->bitmapinfoheader = malloc(sizeof(byte)*40);//Need to allocate space for bitmapinfoheader
-	fread(image -> header -> bitmapinfoheader,sizeof( image -> header -> bitmapinfoheader), 40, file);
+	fread(image -> header -> bitmapinfoheader,sizeof(byte), 40, file);
 	unsigned int width = (unsigned int)(image ->header->bitmapinfoheader[4]),height=(unsigned int)(image ->header->bitmapinfoheader[8]);
 	printf("%ux%u\n",width,height);
 	return image;
