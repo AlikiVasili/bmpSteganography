ad490a183d9c79e4e40f9818d8fff0565fcf6abc
AlikiVasili <avasil08@ucy.ac.cy>
2020-11-18 15:31:57 +0200

    i started working on grayscale function.

---
+++ grayscale.c
@@ -0,0 +1,18 @@
+#include 'grayscale.h'
+
+void grayscale_filter(FILE img){
+	
+}
+
+int calculate_luminance(byte red,byte green,byte blue){
+	int luminance  = 0, ntsc = 0;
+}
+
+void change_pixel(FILE img){
+	
+}
+
+#ifdef DEBUG2
+int main(){
+}
+#endif
