9bc52f163e11b0276def9932659060303e5e15e5
AlikiVasili <avasil08@ucy.ac.cy>
2020-11-18 17:53:49 +0200

    i read and the rest of the image

--- image.c
+++ image.c
@@ -6,7 +6,6 @@ IMAGE * initImage(FILE *file,char *fileName){
 	image->header = (HEADER *)malloc(sizeof(HEADER)); //Need space for header
 	image -> header -> bmpFileHeader =(BMPFILEHEADER *) malloc(sizeof(BMPFILEHEADER));
 	image -> header -> bmpInfoHeader =(BMPINFOHEADER *) malloc(sizeof(BMPINFOHEADER));
-
 	
 	if(image == NULL){
 		printf("Reseve space in memory can't be done");
@@ -25,6 +24,29 @@ IMAGE * initImage(FILE *file,char *fileName){
 	fread(&(image -> header -> bmpFileHeader ->bfOffBits),sizeof(dword), 1, file);
 
 	fread(image -> header -> bmpInfoHeader,sizeof(BMPINFOHEADER), 1, file);
+	
+	//see if there are padding pixels
+	int padding_pixels = 0;
+	int temp = 0;
+	temp = ((image -> header -> bmpInfoHeader -> biWidth)*3) % 4;
+	if(temp != 0){
+		padding_pixels = 4 - temp;
+		padding_pixels = padding_pixels * (image -> header -> bmpInfoHeader -> biHeight);
+	}
+	
+	//find how much pixels the image has
+	int how_much_pixels = (image -> header -> bmpInfoHeader -> biWidth) * (image -> header -> bmpInfoHeader -> biHeight);
+	how_much_pixels = how_much_pixels + padding_pixels;
+	//reseve space in memory from the pixels
+	image -> pixels = (PIXEL *)malloc(how_much_pixels * sizeof(PIXEL));
+	
+	int i = 0;
+	for(i = 0; i<how_much_pixels ; i++){
+		fread(&(image -> pixels[i].byte1), sizeof(byte), 1 , file);
+		fread(&(image -> pixels[i].byte2), sizeof(byte), 1 , file);
+		fread(&(image -> pixels[i].byte3), sizeof(byte), 1 , file);
+	}
+	
 	return image;
 }
 
@@ -32,7 +54,6 @@ IMAGE * initImage(FILE *file,char *fileName){
 int main(){
 	FILE *f = NULL;
 	IMAGE *img = initImage(f,"4x3.bmp");
-	printf("%c%c ", img -> header -> bitmapfileheader[0],  img -> header -> bitmapfileheader[1]); //print BM
-	printf("%dBytes\n", img -> header -> bitmapfileheader[2]);//print Size of file 
+	printf("%c\n", img -> pixels[0].byte2);
 }
 #endif
