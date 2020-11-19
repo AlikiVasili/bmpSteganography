956cbf027cb6450f2287df92c7c5a43b88d64d32
Panagiotis-Papadopoulos <ppapad03@ucy.ac.cy>
2020-11-19 14:48:56 +0200

    Finished listImg.h

--- listImg.h
+++ listImg.h
@@ -2,3 +2,9 @@
  *  @author Panagiotis Papadopoulos
  */ 
 #include "image.h"
+/**
+ * @brief This function takes a bmp image as an argument and proceeds to print it's header. 
+ * 
+ * @param IMAGE_*_img The Image whose header we want to print.
+ */
+void list(IMAGE *img);
