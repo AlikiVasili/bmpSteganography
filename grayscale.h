210b2471dd1da5f373bf6015f3efbd7460d220f5
AlikiVasili <avasil08@ucy.ac.cy>
2020-11-20 10:57:36 +0200

    Finished the grayscale filter

--- grayscale.h
+++ grayscale.h
@@ -1,5 +1,5 @@
 /** @file grayscale.h
- *  @brief This is a header file that haa the functions in order to create a grayscale image.
+ *  @brief This module turn an image to its grayscale version.
  *
  *  @author Aliki Vasili
  *  @bug No known bugs.
@@ -9,10 +9,26 @@
 
 #include "image.h"
 
+/**
+ * @brief This function turn the given image in its grayscale vesion.
+ * 
+ * @param image The bmp image which will be turn into grayscale.
+ * @param file The file name of the bmp file that has the image.
+ */
 void grayscale_filter(IMAGE *image, char *file);
+/**
+ * @brief This function calculate the luminance of a pixel.
+ * 
+ * @param red The first(red) byte of the pixel.
+ * @param greem The second(green) byte of the pixel.
+ * @param blue The third(blue) byte of the pixel.
+ */
 int calculate_luminance(byte red,byte green,byte blue);
-//bytes * read_pixel(IMAGE img);
-void change_pixel(PIXEL pixel);
+/**
+ * @brief This function rounds the given number.
+ * 
+ * @param x The number we want to round.
+ */
 int my_round(double x);
 
 #endif
