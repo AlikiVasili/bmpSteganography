8c1adc140b7e303a71a47fcd6884c942da62deb0
Panagiotis-Papadopoulos <ppapad03@ucy.ac.cy>
2020-11-21 18:45:48 +0200

    Added comments for library functions.

--- image.h
+++ image.h
@@ -49,12 +49,56 @@ typedef struct image{
 	int padding_pixels;
 }IMAGE;
 
+/**
+ * @brief This function opens a given file and
+ * save it in an IMAGE struct if it's a bmp image.
+ * 
+ * @param fileName The name of the file to be opened.
+ * @return IMAGE* The pointer to that IMAGE struct.
+ */
 IMAGE *initImage(char *fileName);
+/**
+ * @brief This function deletes an IMAGE struct from memory.
+ * 
+ * @param img The IMAGE to be deleted.
+ */
 void deleteImage(IMAGE *img);
+/**
+ * @brief This function checks if a given IMAGE is a bmp file or not.
+ * 
+ * @param img THE IMAGE to be cjecked.
+ * @return int 1 if the IMAGE is a bmp file 0 if otherwise.
+ */
 int isBmp(IMAGE *img);
+/**
+ * @brief This function checks if a given IMAGE is a bmp file
+ * with no compression and 24-bit color depth.
+ * 
+ * @param img The IMAGE to be checked.
+ * @return int 1 if there is no compression and 
+ * the color depth is 24-bits, 0 if otherwise.
+ */
 int isUncompressed24bit(IMAGE *img);
+/**
+ * @brief This function creates a copy of a given IMAGE.
+ * 
+ * @param src The IMAGE to be copied.
+ * @return IMAGE* The copy of the given IMAGE or NULL in case of failure.
+ */
 IMAGE *copyImage(const IMAGE *src);
+/**
+ * @brief This function saves an IMAGE in a file with a given filename.
+ * 
+ * @param src The IMAGE to be saved.
+ * @param imageName The filename.
+ */
 void saveImage(const IMAGE *src, char *imageName);
+/**
+ * @brief This function retruns the amount of pixels in the IMAGE struct.
+ * 
+ * @param src The IMAGE whose pixel amount to get.
+ * @return int The amount of pixels including padding pixels.
+ */
 int getPixelAmount(const IMAGE *src);
 
 #endif
